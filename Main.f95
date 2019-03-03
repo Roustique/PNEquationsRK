cf4411eeea7c9287e520d13a28d59defdd3b2e01
Roustique <roustique.g@gmail.com>
2019-03-04 00:48:45 +0300

    Beta 1.7 (Renormalization and adding G)

--- Main.f95
+++ Main.f95
@@ -4,18 +4,18 @@ use RungeKuttaPNEquations
 implicit none
 real(mp), dimension(:,:), allocatable :: Coord, Velocity
 real(mp), dimension(:), allocatable :: Energy
-real(mp) :: step, GM, E0
+real(mp) :: step, Mass, E0
 integer(4) :: amount, i
 real(mp), dimension(2) :: Coord0, Velocity0
 character(2) :: Key
 
 call getarg(1,Key)
 
-open(1,file="data.dat")
+open(1,file="./input/data.dat")
 
  read(1,*)step, amount
  read(1,*)Coord0(1), Coord0(2), Velocity0(1), Velocity0(2)
- read(1,*)GM
+ read(1,*)Mass
 
 close(1)
 
@@ -24,14 +24,14 @@ Velocity0=Velocity0
 
 allocate(Coord(0:amount,2),Velocity(0:amount,2),Energy(0:amount))
 
-call RKIntegrate(step, amount, Coord0, Velocity0, GM, Coord, Velocity, Energy, Key)
+call RKIntegrate(step, amount, Coord0, Velocity0, Mass, Coord, Velocity, Energy, Key)
 
-open(2,file="result.dat")
+open(2,file="./output/result.dat")
 
-write(2,*)"x, y, vx, vy, E"
+write(2,*)"x, y, vx, vy, E, L"
 do i=0,amount
-   write(2,*)Coord(i,1)*2.95e+5,", ", Coord(i,2)*2.95e+5,", ", Velocity(i,1)*2.95e+5,", ",&
-   Velocity(i,2)*2.95e+5, ", ",Energy(i)
+   write(2,*)Coord(i,1),", ", Coord(i,2),", ", Velocity(i,1),", ",&
+   Velocity(i,2), ", ",Energy(i), ", ", Coord(i,1)*Velocity(i,2)-Coord(i,2)*Velocity(i,1)
 enddo
 
 close(2)
--- Runge-Kutta.f95
+++ Runge-Kutta.f95
@@ -1,35 +1,37 @@
 module RungeKuttaPNEquations
 use my_prec
 implicit none
+real(mp), parameter :: c=1.0 !Speed of light 
+real(mp), parameter :: G=1.47662 !Gravitational Parameter
 contains
 
-subroutine EqMotPostNewt(x,v,GM,c,k)
+subroutine EqMotPostNewt(x,v,Mass,k)
 real(mp), intent(in), dimension(2) :: x, v
-real(mp), intent(in) :: GM, c
+real(mp), intent(in) :: Mass
 real(mp), intent(out), dimension(4) :: k
 real(mp) :: C1, C2, r
 
     r=sqrt(x(1)**2+x(2)**2)
     
-    C1=-(c*c+v(1)*v(1)+v(2)*v(2)-4*GM/r)
+    C1=-(c*c+v(1)*v(1)+v(2)*v(2)-4*G*Mass/r)
     C2=4*(v(1)*x(1)+v(2)*x(2))
 
     k(1)=v(1)
     k(2)=v(2)
-    k(3)=(C1*x(1)+C2*v(1))*GM/(c*c*r**3)
-    k(4)=(C1*x(2)+C2*v(2))*GM/(c*c*r**3)
+    k(3)=(C1*x(1)+C2*v(1))*G*Mass/(c*c*r**3)
+    k(4)=(C1*x(2)+C2*v(2))*G*Mass/(c*c*r**3)
 
 end subroutine EqMotPostNewt
 
-subroutine EqMotNewt(x,v,GM,k)
+subroutine EqMotNewt(x,v,Mass,k)
 real(mp), intent(in), dimension(2) :: x, v
-real(mp), intent(in) :: GM
+real(mp), intent(in) :: Mass
 real(mp), intent(out), dimension(4) :: k
 real(mp) :: gradphi, r
 
 	r=sqrt(x(1)**2+x(2)**2)
 
-	gradphi=-GM/r**3
+	gradphi=-G*Mass/r**3
 
     k(1)=v(1)
 	k(2)=v(2)
@@ -38,14 +40,14 @@ real(mp) :: gradphi, r
 
 end subroutine EqMotNewt
 
-subroutine RKIntegrate(step, amount, Coord0, Velocity0, GM, Coord, Velocity, Energy, Key)
-real(mp), intent(in) :: step, GM
+subroutine RKIntegrate(step, amount, Coord0, Velocity0, Mass, Coord, Velocity, Energy, Key)
+real(mp), intent(in) :: step, Mass
 integer(4), intent(in) :: amount
 real(mp), intent(in), dimension(2) :: Coord0, Velocity0   !Initial Values 
 real(mp), intent(out), dimension(0:amount,2) :: Coord, Velocity
 real(mp), intent(out), dimension(0:amount) :: Energy
-real(mp), parameter :: c=101624.562 !Speed of light 
 real(mp) :: r  !r is module of Coord vector
+real(mp) :: v2 !v2 is scalar square of velocity
 real(mp), dimension(4,4) :: k  !RK4 coefficients
 integer(4) :: i
 character(2), intent(in) :: Key
@@ -53,23 +55,33 @@ character(2), intent(in) :: Key
     Coord(0,:)=Coord0(:)
     Velocity(0,:)=Velocity0(:)
     r=sqrt(sum(Coord0**2))
-    Energy(0)=sum(Velocity(0,:)**2)/2-GM/r!+(GM/r)**2/(2*c**2)
+	v2=sum(Velocity(0,:)**2)
+
+	if (Key == "-P") then
+
+        Energy(0)=v2/2-G*Mass/r-(1/c**2)*(v2**2/8+3*G*Mass/2/r*v2-(G*Mass/r)**2/2)
+
+	elseif (Key == "-N") then
+
+		Energy(0)=v2/2-G*Mass/r
+
+	endif
     
     do i=1,amount
        
 		if (Key=="-P") then
 
-        	call EqMotPostNewt(Coord(i-1,:),Velocity(i-1,:),GM,c,k(1,:))
-        	call EqMotPostNewt(Coord(i-1,:)+step*k(1,:2)/2,Velocity(i-1,:)+step*k(1,3:)/2,GM,c,k(2,:))
-        	call EqMotPostNewt(Coord(i-1,:)+step*k(2,:2)/2,Velocity(i-1,:)+step*k(2,3:)/2,GM,c,k(3,:))
-        	call EqMotPostNewt(Coord(i-1,:)+step*k(3,:2),Velocity(i-1,:)+step*k(3,3:),GM,c,k(4,:))
+        	call EqMotPostNewt(Coord(i-1,:),Velocity(i-1,:),Mass,k(1,:))
+        	call EqMotPostNewt(Coord(i-1,:)+step*k(1,:2)/2,Velocity(i-1,:)+step*k(1,3:)/2,Mass,k(2,:))
+        	call EqMotPostNewt(Coord(i-1,:)+step*k(2,:2)/2,Velocity(i-1,:)+step*k(2,3:)/2,Mass,k(3,:))
+        	call EqMotPostNewt(Coord(i-1,:)+step*k(3,:2),Velocity(i-1,:)+step*k(3,3:),Mass,k(4,:))
 
 		elseif (Key=="-N") then
 
-			call EqMotNewt(Coord(i-1,:),Velocity(i-1,:),GM,k(1,:))
-        	call EqMotNewt(Coord(i-1,:)+step*k(1,:2)/2,Velocity(i-1,:)+step*k(1,3:)/2,GM,k(2,:))
-        	call EqMotNewt(Coord(i-1,:)+step*k(2,:2)/2,Velocity(i-1,:)+step*k(2,3:)/2,GM,k(3,:))
-        	call EqMotNewt(Coord(i-1,:)+step*k(3,:2),Velocity(i-1,:)+step*k(3,3:),GM,k(4,:))
+			call EqMotNewt(Coord(i-1,:),Velocity(i-1,:),Mass,k(1,:))
+        	call EqMotNewt(Coord(i-1,:)+step*k(1,:2)/2,Velocity(i-1,:)+step*k(1,3:)/2,Mass,k(2,:))
+        	call EqMotNewt(Coord(i-1,:)+step*k(2,:2)/2,Velocity(i-1,:)+step*k(2,3:)/2,Mass,k(3,:))
+        	call EqMotNewt(Coord(i-1,:)+step*k(3,:2),Velocity(i-1,:)+step*k(3,3:),Mass,k(4,:))
 
 		else
 
@@ -81,8 +93,17 @@ character(2), intent(in) :: Key
         Velocity(i,:)=Velocity(i-1,:)+step/6*(k(1,3:)+2*k(2,3:)+2*k(3,3:)+k(4,3:))
  
         r=sqrt(sum(Coord(i,:)**2))
+		v2=sum(Velocity(i,:)**2)
 
-        Energy(i)=sum(Velocity(i,:)**2)/2-GM/r!+(GM/r)**2/(2*c**2)
+		if (Key == "-P") then
+
+        	Energy(i)=v2/2-G*Mass/r-(1/c**2)*(v2**2/8+3*G*Mass*v2/(2*r)-(G*Mass/r)**2/2)
+
+		elseif (Key == "-N") then
+
+			Energy(i)=v2/2-G*Mass/r
+
+		endif
 
     enddo
 
